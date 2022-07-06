926f47ac84d3bd81ac031754a6104659d3dbf28e
Saker-Hossain <sakerhossain2017@gmail.com>
2022-07-06 12:58:09 +0600

    Initial Commit

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +19,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::apiResource('/products', ProductController::class);
+
+Route::group(['prefix' => 'products'],function(){
+   Route::apiResource('/{product}/reviews', ReviewController::class);
 });
